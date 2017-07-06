d1e9199a010969edfb58d57dfd62d83cf79ba536
Pernfuß, Jörg <code.jpe@gmail.com>
2017-07-06 11:57:14 +0200

    Refactor functions for zk path creation

--- zookeeper.go
+++ zookeeper.go
@@ -8,6 +8,16 @@
 
 package main // import "github.com/mjolnir42/zkonce"
 
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/samuel/go-zookeeper/zk"
+)
+
 func connect(cstr string) (*zk.Conn, string) {
 	var servers, chroot string
 	sr := strings.SplitN(cstr, `/`, 2)
@@ -26,6 +36,38 @@ func connect(cstr string) (*zk.Conn, string) {
 	assertOK(err)
 
 	return conn, chroot
+}
+
+func zkHier(conn *zk.Conn, hier string, existsOK bool) bool {
+	hierParts := strings.Split(hier, `/`)
+
+	for i := range hierParts {
+		part := filepath.Join(append(
+			[]string{`/`}, hierParts[0:i+1]...)...)
+
+		// root always exists
+		if part == `/` {
+			continue
+		}
+		// create node
+		if !zkCreatePath(conn, part, existsOK) {
+			return false
+		}
+	}
+	return true
+}
+
+func zkCreatePath(conn *zk.Conn, path string, existsOK bool) bool {
+	createdPath, err := conn.Create(path, []byte{}, int32(0), zk.WorldACL(zk.PermAll))
+	if err != zk.ErrNodeExists || !existsOK {
+		if errorOK(err) {
+			return false
+		}
+	}
+	if createdPath != `` {
+		logrus.Infof("Created zk node %s", createdPath)
+	}
+	return true
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
